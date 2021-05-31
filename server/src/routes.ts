0a6095a15e0a896ab5982ecfdac14aaccf8e5b19
Alex Junior Pereira <alexjp17@gmail.com>
2021-05-31 00:32:19 -0300

    removido funcao de test das rotas

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,12 +1,7 @@
-import express, {Request, Response} from 'express'
+import express from 'express'
 import { getProdutos, postProduto, putProduto, deleteProduto } from './controllers/firebaseController'
 
 const routes = express.Router()
-
-function test(text: string, req: Request, res: Response){
-    console.log(text)
-    res.send("ok")
-}
 
 routes.get('/produtos', getProdutos)
 routes.post('/produtos', postProduto)
